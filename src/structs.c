d128bb45454d154b2e64c7c0d65b5651cad00d35
LUISCDF <luiscdf@ua.pt>
2019-10-28 18:51:53 +0000

    so um quick update pa ti mendes

--- src/structs.c
+++ src/structs.c
@@ -215,7 +215,84 @@ ImageRGB * read_rgb(char *file_name)
 
 ImageBin * read_bin(char *file_name) 
 {
- 	   
+    FILE *fp = fopen(file_name, "r");
+	if (fp == NULL){
+		printf("%s\n", "Erro na abertura do ficheiro!");
+        exit(EXIT_FAILURE);
+    }
+
+    /**
+    Variable to store the file type;
+    */
+    char *type = (char *) malloc(3 * sizeof(char));
+    fscanf(fp, "%s", type);
+    printf("%s\n", type);
+    if (strcmp(type,"P4") != 0){	//TODO: TESTAR
+    	printf("%s\n", "Formato de ficheiro inválido!");
+        exit(EXIT_FAILURE);
+    }
+
+    /**
+	Store image dimentions;
+    */
+    int *larg = (int *)malloc(1 * sizeof(int)), *comp = (int *)malloc(1 * sizeof(int));
+    fscanf(fp, "%d %d", larg, comp);
+    printf("%d x %d\n", *larg, *comp);
+
+    /**
+	Store color range;
+    Note: Not needed!!
+    */
+    /* int *colorRange = (int *)malloc(1 * sizeof(int));
+    fscanf(fp, "%d", colorRange);
+    printf("%d\n", *colorRange); */
+
+    ImageRGB* image = create_imageBin(*larg, *comp);  //TODO: nao sei se isto funciona
+
+    //END of first lecture (as a ordinary text file)
+    long offset = ftell(fp);
+    printf("offsef: %ld\n", ftell(fp));
+    fclose(fp);
+
+    //BEGINNING of second lecture (as a binary file)
+    fp = fopen(file_name, "rb");
+    fseek(fp, offset + 1, SEEK_SET);
+
+    //array of rbg colors;
+    unsigned char rgbArray[3];
+    
+    //variable to store temporary color;
+    unsigned char *needsChar = (unsigned char*) malloc(sizeof(unsigned char));
+    
+    //variable to keep track of current rbgArray (TODO POR LINK PA ESSA VAR, se possivel) index;
+    int index = -1;
+
+    //TODO debug
+    long rounds2 = 0; 
+
+    //initializations
+    int bytesread = 0;
+    int bytes_count = 0;
+    while ((bytesread = fread(needsChar, sizeof(*needsChar), 1, fp)) > 0){
+        
+        //ciclying the index, never letting it getting past 2, preventing overflows
+        rgbArray[index = ++index % 3] = *needsChar;
+       	
+       	//after a complete pixel read (aka 3 bytes), do some extra work...
+        if (index == 2){
+
+         //   printf("%ld: %u - %u - %u\n",rounds2++, rgbArray[0], rgbArray[1],rgbArray[2]);
+            RGBPixel *temp_pixel = create_rgb_pixel(rgbArray);
+            image->a[bytes_count++] = *temp_pixel;
+
+            
+        }
+    }
+    printf("[INFO]: %s; BytesCounted: %d", "Done", bytes_count);
+    printf("[TODO]>>> %u", image->a[bytes_count - 1].rgb[0]);
+    printf("[TODO 2]>>> %d", image->heigth);
+    return image;
+
 }
 
 
@@ -288,7 +365,7 @@ ImageGray* convert_rbgToGrayParametized(ImageRGB* image, char* color){
 
 }
 
-void apply_filter_toRGB(ImageRGB* image, float filter[9]){
+void apply_filter_toRGB(ImageRGB* image, float filter[9]){ // TODO: por malloc, pa n inicizalizar
     RGBPixel *temp_a = (RGBPixel *)calloc(image->length * image->heigth, sizeof(GrayPixel));
     int total_cols = image->length;
     int count = 0;
@@ -351,8 +428,8 @@ int main()
     float filter2[9] = {-1,-1,-1,
                         -1,8,-1,
                         -1,-1,-1};
-    apply_filter_toRGB(imagem2, filter2);
-    write_rgb(imagem2, "filtros3.ppm");
+    /* apply_filter_toRGB(imagem2, filter);
+    write_rgb(imagem2, "filtros3.ppm"); */
 
 
     /* ImageGray* imagem = read_gray("lena2.ppm");
