55a237c029d53d92116dee509c514c01b68f6726
LUISCDF <luiscdf@ua.pt>
2019-10-29 12:17:27 +0000

    mais um teste

--- src/structs.c
+++ src/structs.c
@@ -1008,14 +1008,14 @@ int main()
     apply_filter_toRGB(i1, edgeDetection1);
 
     printf("\nSaving new RGB image to a file...\n");
-    write_rgb(gray_image, "../out/p06-1.ppm");
+    write_rgb(i1, "../out/p06-1.ppm");
     printf("\nImage saved with sucess!\n");
 
     printf("\nApplying filter to Grayscale image...\n");
-    (i1, edgeDetection1);
+    apply_filter_toGray(gray_image, edgeDetection1);
 
     printf("\nSaving new grayscale image to a file...\n");
-    write_gray(gray_image, "../out/p06-1.ppm");
+    write_gray(gray_image, "../out/p06-2.ppm");
     printf("\nImage saved with sucess!\n");
 
 
